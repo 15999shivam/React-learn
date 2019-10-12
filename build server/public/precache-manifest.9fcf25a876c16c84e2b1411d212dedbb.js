c2b46a0570245a2bbe6ab6dfaee0b8ea6ab87eff
Shivam <shivamgarg260@gmail.com>
2019-10-12 23:16:35 +0530

    made a simple build server to test build of each react app build files

---
+++ build server/public/precache-manifest.9fcf25a876c16c84e2b1411d212dedbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5deab2924b172a6c4635e83dd459d95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85dd89eaadac4544319b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2bf9066949b79ddd3ace",
+    "url": "/static/js/2.00dacc0a.chunk.js"
+  },
+  {
+    "revision": "85dd89eaadac4544319b",
+    "url": "/static/js/main.f809fedc.chunk.js"
+  },
+  {
+    "revision": "58fce970880413218987",
+    "url": "/static/js/runtime-main.0744fe7a.js"
+  }
+]);
